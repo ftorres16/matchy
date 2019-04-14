73f12224a4cb5cd7cc027c2ea7a5db74f532a823
fabian <fabian.torres.alvarez+github@gmail.com>
2019-04-14 17:08:52 -0300

    random search

--- matchy/matching_functions.py
+++ matchy/matching_functions.py
@@ -3,6 +3,9 @@ import string
 
 import numpy as np
 
+MAX_TRIES = 10000
+TOL = 1e-6
+
 
 def match(n, m):
     """
@@ -19,6 +22,44 @@ def match(n, m):
     n_spares = L ** 2 - len(flattened_names)
     flattened_names += ["?"] * n_spares
 
-    match_matrix = np.array(flattened_names).reshape((L, L))
+    match_matrix = np.array(flattened_names).reshape(L, L)
+
+    # introduce some randomness to make it faster
+    match_matrix = match_matrix.ravel()
+    np.random.shuffle(match_matrix)
+    match_matrix = match_matrix.reshape(L, L)
+
+    error = get_error(match_matrix)
+    num_tries = 0
+    random_matrix = np.copy(match_matrix)
+    while error > TOL and num_tries < MAX_TRIES:
+        # random search
+        random_matrix = random_matrix.ravel()
+        np.random.shuffle(random_matrix)
+        random_matrix = random_matrix.reshape(L, L)
+
+        if get_error(random_matrix) < error:
+            match_matrix = np.copy(random_matrix)
+
+        num_tries += 1
 
     return match_matrix
+
+
+def get_error(mat, dummy_name="?"):
+    """
+    Returns the root of the square sum of the centroids for all the elements
+    in `mat` while ignoring the dummies.
+    """
+    names = np.unique(mat)
+    names = names[names != dummy_name]
+
+    centroids = np.zeros((len(names), 2))
+
+    for index, name in enumerate(names):
+        row_vals, col_vals = np.where(mat == name)
+        center_y = np.mean(row_vals) - (len(row_vals) - 1) / 2
+        center_x = np.mean(col_vals) - (len(col_vals) - 1) / 2
+        centroids[index] = center_x, center_y
+
+    return np.sqrt(np.sum(centroids ** 2))
