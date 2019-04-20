69e94df425e8655585c33d169d93007a20ebbced
fabian <fabian.torres.alvarez+github@gmail.com>
2019-04-20 16:44:27 -0300

    Now accepting initial guesses

--- matchy/matching_functions.py
+++ matchy/matching_functions.py
@@ -14,30 +14,37 @@ TOL = 1e-6
 METHODS = {"random": random_search, "hill_climbing": hill_climbing}
 
 
-def match(n, m, method="random"):
+def match(n=None, m=None, method="random", initial_guess=None):
     """
     Returns a matrix with the best possible matching for N devices,
     where there multiplicities are given by the elements in M.
+
+    Optionally, you can pass an initial guess matrix that will attempt
+    to be matched. In this case, M and N are ignored.
     """
-    # get all possible names
-    names = [string.ascii_uppercase[i] for i in range(n)]
-    # get a list where each member is a piece of each device
-    flattened_names = [
-        name for index, name in enumerate(names) for _ in range(m[index])
-    ]
+    if initial_guess is not None:
+        match_matrix = initial_guess
+        names = get_device_names(match_matrix)
+    else:
+        # get all possible names
+        names = [string.ascii_uppercase[i] for i in range(n)]
+        # get a list where each member is a piece of each device
+        flattened_names = [
+            name for index, name in enumerate(names) for _ in range(m[index])
+        ]
 
-    # get the lenght of the square where the devices will be laid
-    L = math.ceil(math.sqrt(len(flattened_names)))
+        # get the lenght of the square where the devices will be laid
+        L = math.ceil(math.sqrt(len(flattened_names)))
 
-    # add spare devices as needed
-    n_spares = L ** 2 - len(flattened_names)
-    flattened_names += ["?"] * n_spares
+        # add spare devices as needed
+        n_spares = L ** 2 - len(flattened_names)
+        flattened_names += ["?"] * n_spares
 
-    match_matrix = np.array(flattened_names)
+        match_matrix = np.array(flattened_names)
 
-    # introduce some randomness to make it faster
-    np.random.shuffle(match_matrix)
-    match_matrix = match_matrix.reshape(L, L)
+        # introduce some randomness to make it faster
+        np.random.shuffle(match_matrix)
+        match_matrix = match_matrix.reshape(L, L)
 
     match_matrix = METHODS[method](match_matrix, names=names)
 
