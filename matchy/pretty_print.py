259edae3c2325a046b3f957dfb8f3cc9a6f41efe
Fabian Torres <fabian.torres.alvarez@gmail.com>
2019-09-01 01:07:27 -0300

    modularized pretty print

---
+++ matchy/pretty_print.py
@@ -0,0 +1,46 @@
+import click
+
+
+def pretty_print_matrix(mat):
+    """
+    Pretty print a 2D numpy array.
+
+    For example, if `mat` is [['A', 'B'],
+                              ['B', 'A']]
+
+    Then the output will print:
+        ┌─────┐
+        │ A B │
+        │ B A │
+        └─────┘
+    """
+    box_top = "─" * (2 * mat.shape[1] + 1)
+    click.echo(f"┌{box_top}┐")
+    for row in mat:
+        devices = " ".join(row)
+        click.echo(f"│ {devices} │")
+    click.echo(f"└{box_top}┘")
+
+
+def pretty_print_table(headers, rows):
+    col_width = len(max(headers, key=len))
+    num_cols = len(headers)
+
+    box_top = "─┬─".join(["─" * col_width for _ in range(num_cols)])
+    headers_line = " │ ".join([f"{header:>{col_width}}" for header in headers])
+    separator = "─┼─".join(["─" * col_width for _ in range(num_cols)])
+    row_separator = "┈┼┈".join(["┈" * col_width for _ in range(num_cols)])
+    box_bot = "─┴─".join(["─" * col_width for _ in range(num_cols)])
+
+    click.echo(f"┌─{box_top}─┐")
+    click.echo(f"│ {headers_line} │")
+    click.echo(f"├─{separator}─┤")
+
+    for index, row in enumerate(rows):
+        if index > 0:
+            click.echo(f"├┈{row_separator}┈┤")
+
+        row = " │ ".join([f"{value:>{col_width}}" for value in row])
+        click.echo(f"│ {row} │")
+
+    click.echo(f"└─{box_bot}─┘")
